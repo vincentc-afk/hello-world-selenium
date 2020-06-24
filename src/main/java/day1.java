a02a98b2e9cb87bb8ede82504d3a254a09901a2e
Herve <dockerlite@gmail.com>
2020-06-24 11:36:43 +0200

    change ip address

--- src/main/java/day1.java
+++ src/main/java/day1.java
@@ -30,7 +30,7 @@ public class day1 {
     public void searchString() {
         // find search box and enter search string
         String pagecontent = driver.getPageSource();
-        String search="TEAM";
+        String search="AFIP";
         if (pagecontent.toLowerCase().indexOf(search.toLowerCase()) != -1) {
             System.out.println("OK");
         }else {
